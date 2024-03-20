91780035635652569d2a15197870f32a4f9c8c5a
Nsmith4789 <156531219+Nsmith4789@users.noreply.github.com>
2024-03-19 19:12:39 -0500

    updated list style and add event listeners

---
+++ script.js
@@ -0,0 +1,8 @@
+button2.onclick = function () {
+    alert("In 2023 he became the NBA's all-time leading scorer, breaking the record (38,387) previously held by Kareem Abdul-Jabbar")
+    BookNow.style.color = "gold";
+    BookNow.style.padding = "10px"
+}
+button3.onclick = function () {
+    alert("He has also won a total of 6 NBA championships for the Chicago Bulls")
+}
